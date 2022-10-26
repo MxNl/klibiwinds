ca01c0f1ac9cfe0e19f4941e5e56ec44ee4e4655
MxNl <maxnoelscher@posteo.de>
2022-10-26 14:39:25 +0200

    wip

--- R/make_plot_distribution_vs.R
+++ R/make_plot_distribution_vs.R
@@ -29,19 +29,28 @@ make_plot_distribution_vs <- function(plot_data, var_rows, var_columns, absolute
     stringr::str_replace("_", " ") |>
     stringr::str_to_sentence()
 
-    column_selector <- absolute_value_or_change
-    string_to_remove <- stringr::str_glue("absolute_{absolute_value_or_change}_z")
+  column_selector <- absolute_value_or_change
+  string_to_remove <- stringr::str_glue("absolute_{absolute_value_or_change}_z")
 
-  plot_data |>
+  plot_data <- plot_data |>
     dplyr::rename(absolute_value_z1 = observed) |>
     tidyr::pivot_longer(cols = dplyr::contains(column_selector), names_to = "period") |>
     dplyr::mutate(period = period |> stringr::str_remove(string_to_remove)) |>
     z_to_yearrange_period_names() |>
+    dplyr::group_by(!!var_columns, !!var_rows) |>
+    dplyr::mutate(n_obs = paste(
+      dplyr::n() / length(unique(period))
+      # " / ",
+      # n() / length(unique(period)) / n() / length(unique(period))
+    )) |>
+    ungroup()
+
+  plot_data |>
     ggplot2::ggplot(
-      ggplot2::aes(value, period, height = ..density.., fill = period)
+      ggplot2::aes(value, period)
     ) +
     ggridges::geom_density_ridges(
-      ggplot2::aes(colour = period),
+      ggplot2::aes(colour = period, fill = period, height = ..density..),
       quantile_lines = TRUE,
       quantiles = 2,
       size = .4,
@@ -50,6 +59,20 @@ make_plot_distribution_vs <- function(plot_data, var_rows, var_columns, absolute
       # colour = "white",
       scale = 2.5,
       show.legend = FALSE
+    ) +
+    ggplot2::geom_text(
+      data = plot_data |> dplyr::distinct(!!var_columns, !!var_rows, .keep_all = TRUE),
+      ggplot2::aes(
+        label = paste(n_obs, "obs."),
+        y = Inf,
+        x = -Inf
+      ),
+      size = 2,
+      fontface = "bold",
+      family = "base_font",
+      colour = "grey70",
+      vjust = 2,
+      hjust = 0
     ) +
     paletteer::scale_colour_paletteer_d("beyonce::X54") +
     paletteer::scale_fill_paletteer_d("beyonce::X54") +
